5df267113ec6fd66cf7c28c1b9619676434653e6
MJstephenson <cyclomark@gmail.com>
2023-01-15 13:30:09 +0000

    Display main-game section with farm btn on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
-//appends an "active" class to .popup-instructions-overlay and .popup-instructions-content when the "instructions" button is clicked
+//Appends an "active" class to .popup-instructions-overlay and .popup-instructions-content when the "instructions" button is clicked
 $(".instruct-btn").on("click", function() {
     $(".popup-instruction-overlay, .popup-instruction-content").addClass("active");
   });
   
-  //removes the "active" class to .popup instructions-overlay and .popup-instructions-content when the "Close" button is clicked 
+  //Removes the "active" class to .popup instructions-overlay and .popup-instructions-content when the "Close" button is clicked 
   $(".close-modal-btn").on("click", function() {
     $(".popup-instruction-overlay, .popup-instruction-content").removeClass("active");
   });
 
-  //appends an "active class to .choose-game when the "start" button is pressed and hides the begin section from the DOM
+  //Appends an "active class to .choose-game when the "start" button is pressed and hides the begin section from the DOM
   $("#start-button").on("click", function() {
     $(".choose-game").addClass("active");
     $(".begin-section").addClass("inactive");
+  });
+
+  //
+  $("#farm-btn").on("click", function() {
+    $(".header-text, .instructions").addClass("inactive");
+    $(".choose-game").removeClass("active");
+    $(".main-game").addClass("active");
   });
