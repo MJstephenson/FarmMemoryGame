eebafcb1ca1dd7c1b6e23753216ca65b9863920d
MJstephenson <cyclomark@gmail.com>
2023-01-17 00:41:40 +0000

    Add reset timer function to reset & new style btns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,92 @@
 //Appends an "active" class to .popup-instructions-overlay and .popup-instructions-content when the "instructions" button is clicked
 $(".instruct-btn").on("click", function() {
-    $(".popup-instruction-overlay, .popup-instruction-content").addClass("active");
-  });
-  
-  //Removes the "active" class to .popup instructions-overlay and .popup-instructions-content when the "Close" button is clicked 
-  $(".close-modal-btn").on("click", function() {
-    $(".popup-instruction-overlay, .popup-instruction-content").removeClass("active");
-  });
+  $(".popup-instruction-overlay, .popup-instruction-content").addClass("active");
+});
 
-  //Appends an "active class to .choose-game when the "start" button is pressed and hides the begin section from the DOM
-  $("#start-button").on("click", function() {
-    $(".choose-game").addClass("active");
-    $(".begin-section").addClass("inactive");
-  });
+//Removes the "active" class to .popup instructions-overlay and .popup-instructions-content when the "Close" button is clicked 
+$(".close-modal-btn").on("click", function() {
+  $(".popup-instruction-overlay, .popup-instruction-content").removeClass("active");
+});
 
-  //On farm game button click, hide all Dom Elements, and only show main game section with farm cards only
-  $("#farm-btn").on("click", function() {
-    $(".header-text, .instructions").addClass("inactive");
-    $(".choose-game").removeClass("active");
-    $(".main-game, .farm-cards").addClass("active");
-  });
+//Appends an "active class to .choose-game when the "start" button is pressed and hides the begin section from the DOM
+$("#start-button").on("click", function() {
+  $(".choose-game").addClass("active");
+  $(".begin-section").addClass("inactive");
+});
 
-  //On space game button click, hide all Dom Elements, and only show main game section with space cards only
-  $("#space-btn").on("click", function() {
-    $(".header-text, .instructions").addClass("inactive");
-    $(".choose-game").removeClass("active");
-    $(".main-game, .space-cards").addClass("active");
-  });
+//On farm game button click, hide all Dom Elements, and only show main game section with farm cards only
+$("#farm-btn").on("click", function() {
+  $(".header-text, .instructions").addClass("inactive");
+  $(".choose-game").removeClass("active");
+  $(".main-game, .farm-cards").addClass("active");
+});
 
-  //On car game button click, hide all Dom Elements, and only show main game section with car cards only
-  $("#cars-btn").on("click", function() {
-    $(".header-text, .instructions").addClass("inactive");
-    $(".choose-game").removeClass("active");
-    $(".main-game, .car-cards").addClass("active");
-  });
+//On space game button click, hide all Dom Elements, and only show main game section with space cards only
+$("#space-btn").on("click", function() {
+  $(".header-text, .instructions").addClass("inactive");
+  $(".choose-game").removeClass("active");
+  $(".main-game, .space-cards").addClass("active");
+});
 
-  //On new game style button click, hide all Dom Elements, and show header, instructions and choose style section
-  $("#new-style-btn").on("click", function() {
-    $(".header-text, .instructions").removeClass("inactive");
-    $(".choose-game").addClass("active");
-    $(".main-game").removeClass("active");
+//On car game button click, hide all Dom Elements, and only show main game section with car cards only
+$("#cars-btn").on("click", function() {
+  $(".header-text, .instructions").addClass("inactive");
+  $(".choose-game").removeClass("active");
+  $(".main-game, .car-cards").addClass("active");
+});
 
-    resetGameStyle()
-  });
+//On new game style button click, hide all Dom Elements, and show header, instructions and choose style section
+$("#new-style-btn").on("click", function() {
+  $(".header-text, .instructions").removeClass("inactive");
+  $(".choose-game").addClass("active");
+  $(".main-game").removeClass("active");
 
- //Add reset game style function to clear the prevous game when choosing a new style of game
-  function resetGameStyle() {
-    const cardsList = ['.farm-cards', '.space-cards', '.car-cards'].forEach(item => {
-    const div = document.querySelector(item);
-    div.classList.remove('active');
-     })
+  resetGameStyle()
+});
 
-    //JQuery Version
-    //const cardsList = ['.farm-cards', '.space-cards', '.car-cards'];
-    //$(cardsList).each(function(item) {
-    // $(cardsList[item]).removeClass('active');
-    //})
-  }
+//Add reset game style function to clear the prevous game when choosing a new style of game
+function resetGameStyle() {
+  const cardsList = ['.farm-cards', '.space-cards', '.car-cards'].forEach(item => {
+  const div = document.querySelector(item);
+  div.classList.remove('active');
+    })
 
-  //timer
-  let timer;
-  let milliseconds = 0;
-  let seconds = 0;
-  let minutes = 0;
+  //JQuery Version
+  //const cardsList = ['.farm-cards', '.space-cards', '.car-cards'];
+  //$(cardsList).each(function(item) {
+  // $(cardsList[item]).removeClass('active');
+  //})
+}
 
-  document.querySelector('.farm-cards').addEventListener('click', startTimer);
+//Add timer
+let timer;
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
 
+document.querySelector('.farm-cards').addEventListener('click', startTimer);//start timer on farm card click
+document.querySelector('#new-style-btn').addEventListener('click', resetTimer);//reset timer on new game style button click
+document.querySelector('#reset-btn').addEventListener('click', resetTimer);//reset timer on reset button click
 
-  function startTimer() {
-      milliseconds++;
+//start timer function
+function startTimer() {
+    milliseconds++;
 
-      if (milliseconds >= 100) {
-          seconds++;
-          milliseconds = 0;
-      }
-  
-      if (seconds >= 60) {
-          minutes++;
-          seconds = 0;
-      }
+    if (milliseconds >= 100) {
+        seconds++;
+        milliseconds = 0;
+    }
 
-      document.querySelector('#timer').innerHTML = `${minutes}:${seconds}:${milliseconds}`;
-      timer = setTimeout(startTimer, 10);
-  }
+    if (seconds >= 60) {
+        minutes++;
+        seconds = 0;
+    }
 
+    document.querySelector('#timer').innerHTML = `${minutes}:${seconds}:${milliseconds}`;
+    timer = setTimeout(startTimer, 10); //start timer after 10 milliseconds
+}
+
+ function resetTimer() {
+        clearInterval(timer); //clears timer https://developer.mozilla.org/en-US/docs/Web/API/clearInterval
+        document.querySelector('#timer').innerHTML = `00:00:00`;
+    }
